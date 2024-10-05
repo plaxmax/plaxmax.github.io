0c947353ebb4942483d6d607eb4b7d5d429f9a48
plaxmax <choquevelascog@gmail.com>
2024-10-05 01:15:04 -0400

    Add files via upload

---
+++ js/imagen.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var parallaxSection = document.getElementById("parallaxSection");
+    var images = ["img/qpfon2.jpg", "img/cat2.jpg", "img/fon1.jpeg"]; // Lista de nombres de las imágenes
+    var currentIndex = 0;
+
+    function changeBackground() {
+        // Cambiar la imagen de fondo
+        parallaxSection.style.backgroundImage = "url('" + images[currentIndex] + "')";
+
+        // Incrementar el índice o reiniciarlo si llegamos al final de la lista
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // Cambiar la imagen cada 5 segundos (5000 milisegundos)
+    setInterval(changeBackground, 5000);
+});
